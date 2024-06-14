71d31ca2d72d876b4dfca556e3e4a3a23a977fc3
lis0517 <01034895017a@gmail.com>
2024-06-14 11:25:49 +0900

    add User

---
+++ src/main/java/com/lys/githubtest/entity/User.java
@@ -0,0 +1,28 @@
+package com.lys.githubtest.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank
+    @Size(min = 10, max = 20)
+    @Column(nullable = false)
+    private String username;
+
+    @NotBlank
+    @Size(min = 10, max = 20)
+    @Column(nullable = false)
+    private String password;
+}
